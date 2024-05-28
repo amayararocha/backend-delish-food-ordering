5a3923f7db61a3c6c29f6d325dd50652d08cb8bb
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-05-28 18:16:35 -0300

    task: implementação do token validador

---
+++ src/main/java/com/delish/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.delish.config;
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY = "jdoisncsnvdinvipeqlvç48948c4682vvdskndiaMSWMFVLF.VDF4V61F6+ER9GRE";
+	public static final String JWT_HEADER = "Authorization";
+}
