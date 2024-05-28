fca34d1f2a103684398c5debde4db8fff8b98f2b
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-05-28 15:20:55 -0300

    task: implementando repository

---
+++ src/main/java/com/delish/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.delish.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.delish.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	public User findByEmail(String username);
+}
