29c243830694692ae97838180fbc6a6e243be24e
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-05-31 17:14:19 -0300

    task: implementando FoodServiceImp

--- src/main/java/com/delish/repository/FoodRepository.java
+++ src/main/java/com/delish/repository/FoodRepository.java
@@ -1,5 +1,19 @@
 package com.delish.repository;
 
-public interface FoodRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.delish.model.Food;
+
+public interface FoodRepository extends JpaRepository<Food, Long>{
+
+	List<Food> findByRestaurantId(Long restaurantId);
+	
+	@Query("SELECT f FROM Food f "
+			+ "WHERE f.name LIKE %:keyword% "
+			+ "OR f.foodCatefory.name LIKE %:keyword%")
+	List<Food> searchFood(@Param("keyword") String keyword);
 }
