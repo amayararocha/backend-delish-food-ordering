7df745cb27e4232e65251e973dc38f5a8a5d1599
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-06-26 18:06:58 -0300

    task: finalizado order API

--- src/main/java/com/delish/controller/AdminOrderController.java
+++ src/main/java/com/delish/controller/AdminOrderController.java
@@ -6,44 +6,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.delish.model.Order;
 import com.delish.model.User;
-import com.delish.request.OrderRequest;
 import com.delish.service.OrderService;
 import com.delish.service.UserService;
 
 @RestController
-@RequestMapping("/api")
-public class OrderController {
-	
+@RequestMapping("/api/admin")
+public class AdminOrderController {
+
 	@Autowired
 	private OrderService orderService;
-	
+
 	@Autowired
 	private UserService userService;
-	
-	@PostMapping("/order")
-	public ResponseEntity<Order> createOrder(@RequestBody OrderRequest req, 
+
+	@GetMapping("/order/restaurant/{id}")
+	public ResponseEntity <List<Order>> getOrderHistory(@PathVariable Long id,
+			@RequestParam(required = false) String order_status,
 			@RequestHeader("Authorization") String jwt) throws Exception {
 
 		User user = userService.findUserByJwtToken(jwt);
-		Order order = orderService.createOrder(req, user);
+		List<Order> orders = orderService.getRestaurantsOrder(id, order_status);
 
-		return new ResponseEntity<>(order, HttpStatus.OK);
+		return new ResponseEntity<>(orders, HttpStatus.OK);
 	}
 
-	@GetMapping("/order/user")
-	public ResponseEntity <List<Order>> getOrderHistory( 
+	@PutMapping("/order/{id}/{orderStatus}")
+	public ResponseEntity <Order> updateOrderStatus(@PathVariable Long id,
+			@PathVariable String orderStatus,
 			@RequestHeader("Authorization") String jwt) throws Exception {
 
 		User user = userService.findUserByJwtToken(jwt);
-		List<Order> orders = orderService.getUserOrder(user.getId());
+		Order orders = orderService.updateOrder(id, orderStatus);
 
 		return new ResponseEntity<>(orders, HttpStatus.OK);
 	}
